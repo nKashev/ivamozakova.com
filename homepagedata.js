5876a9972d64aab7e0655f82e219e2a236a645da
Nikolay Kashev <67652045+nKashev@users.noreply.github.com>
2024-07-09 15:43:47 +0300

    Delete homepagedata.js

--- homepagedata.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Select the element(s) you want to add the attribute to
-    var elements = document.querySelectorAll(".data-home-page");
-
-    // Add the data-home-page attribute to each element
-    elements.forEach(function(element) {
-        element.setAttribute("data-home-page", "https://ivamozakova.com");
-    });
-});
